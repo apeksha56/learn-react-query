8f7218ece0d2497df197d0f566012a263c5de7c7
Apeksha Kumari <apekshasinha562gmail.com>
2023-09-13 20:25:33 +0530

    Initial commit

---
+++ src/pages/Home.js
@@ -0,0 +1,6 @@
+import {useContext, usecontext} from 'react';
+import {AppContext} from '../App';
+export const Home = () => {
+    const { username } = useContext(AppContext);
+    return <h1> This is the home page and user is: {username}</h1>;
+};
---
+++ src/pages/Profile.js
@@ -0,0 +1,16 @@
+import React, { useContext } from 'react';
+import { AppContext } from '../App';
+import {ChangeProfile} from '../components/ChangeProfile';
+
+
+
+export const Profile = () => {
+    const{username} = useContext(AppContext);
+
+    return ( 
+        <div> 
+            PROFILE, user is: {username}
+            <ChangeProfile />
+        </div>
+    );
+};
